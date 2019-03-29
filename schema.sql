772d0487e35a6531b69e438f3b0cdcbb848ee692
Seil Cho <seilcho7@hotmail.com>
2019-03-29 10:06:04 -0400

    adds skeleon

---
+++ schema.sql
@@ -0,0 +1,7 @@
+create table orders (
+    id serial primary key,
+    first_name varchar(100),
+    last_name varchar(100),
+    email varchar(200),
+    coffee_order varchar(200)
+);
---
+++ seed.sql
@@ -0,0 +1,8 @@
+insert into orders
+    (first_name, last_name, email, coffee_order)
+VALUES
+    ('Lady', 'Godiva', 'chocobessed@hotmail.com', 'mocha latte'),
+    ('Nicolas', 'Flamel', 'tomorrowneverdies@netscape.net', 'americano'),
+    ('Cruella', 'DeVille', 'puppypower@yahoo.com', 'ice coffee'),
+    ('Lennie', 'Small', 'bunnybuddy@outlook.com', 'hot chocolate')
+;
